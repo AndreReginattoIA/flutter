32dd2b9ac590e9752db4ddf722afd5f329b629f5
AndreReginattoIA <andre.reginatto@yahoo.com.br>
2020-04-04 21:43:29 -0300

    create private projects for these  deleted projects

--- conversions/lib/models/conversion_abs.dart
+++
@@ -1,6 +0,0 @@
-abstract class ConversionAbs{
-  int index;
-  String description;
-  String toNormal(String value);
-  String fromNormal(String value);
-}
--- conversions/lib/utils/lists.dart
+++
@@ -1,8 +0,0 @@
-import 'package:conversions/models/conversion_abs.dart';
-import 'package:conversions/models/numeric/kilometers.dart';
-import 'package:conversions/models/numeric/meters.dart';
-
-final List<ConversionAbs> teste  = [
-  Meters(),
-  Kilometers(),
-];
